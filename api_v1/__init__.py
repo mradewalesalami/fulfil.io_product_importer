28a1b564a5629674104b3d4ccdfaaea227ee7a31
mradewalesalami <mradewalesalami@gmail.com>
2022-03-05 03:05:24 +0100

    Updated the Blueprint file to include docs. Added a configure module that provides all configuration for the application. Added the application entry interface and initialized the app Updated requirements.txt Created an error module to handle all application errors Created the application factory

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,3 +1,13 @@
+"""
+Blueprint to organize related views.
+URL prefix will be used for all view implementing this blueprint.
+
+E.g, in a view, the route will be in the below format
+@v1_api_product_importer.route('/my_view')
+
+The absolute url will become /api/v1.0/my_view
+"""
+
 from flask import Blueprint
 
 
