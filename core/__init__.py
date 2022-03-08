48a61b17b61ea0b39d386cd266fe192efaa3bc71
mradewalesalami <mradewalesalami@gmail.com>
2022-03-08 03:53:00 +0100

    Commented out the sqlalchemy db create all To use flask migrate to db changes and modifications Updated the webhook url to production url

--- core/__init__.py
+++ core/__init__.py
@@ -60,6 +60,6 @@ def create_app(configuration_mode):
     with app.app_context():
         import api_v1
         import webhooks
-        db.create_all()
+        # db.create_all()
         
     return app, celery
--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,2 @@
-WEBHOOK_URL = 'http://127.0.0.1:2673/api/v1.0/webhook'
+WEBHOOK_URL = 'https://product-importer-rest-api.herokuapp.com/api/v1.0/webhook'
 REQUEST_HEADER = {'Content-Type': 'application/json'}
