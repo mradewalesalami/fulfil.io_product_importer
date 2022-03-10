e0d4dabe5d5710f23ac173f3cd3aab02b8f8d839
mradewalesalami <mradewalesalami@gmail.com>
2022-03-10 22:33:37 +0100

    added aws credentials to .env

--- tasks.py
+++ tasks.py
@@ -91,7 +91,11 @@ def upload_product_from_csv_to_db(self, data):
 
 @celery.task
 def uploadFileS3(file):
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+    )
     # filename = secure_filename(file.filename)
     s3_bucket = 'iamnzapi'
     config = TransferConfig(
