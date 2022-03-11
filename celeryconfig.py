d6a2db0bba6386a1daba4896f2b3b2f8a0e06569
mradewalesalami <mradewalesalami@gmail.com>
2022-03-11 03:18:58 +0100

    updated celeryconfig.py with prod details

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,13 +1,13 @@
 import os
 
-broker_url = 'amqp://celery:celery@localhost:5672/celery'
-# broker_url = os.environ['QUEUE_BROKER_URL']
+# broker_url = 'amqp://celery:celery@localhost:5672/celery'
+broker_url = os.environ['QUEUE_BROKER_URL']
 imports = ('tasks',)
 accept_content = ['msgpack', 'json', 'pickle']
 result_accept_content = ['msgpack', 'json', 'pickle']
 task_serializer = 'pickle'
-result_backend = 'redis://'
-# result_backend = os.environ['REDIS_URL']
+# result_backend = 'redis://'
+result_backend = os.environ['REDIS_URL']
 result_serializer = 'pickle'
 result_expires = None
 # task_time_limit = 60
